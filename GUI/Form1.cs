5e08ffa5d77b9cfba4a66c67ac5864fa2f648df1
trantoan11 <148558306+trantoan11@users.noreply.github.com>
2024-10-30 21:28:38 +0700

    Update Form1.cs

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -57,5 +57,13 @@ namespace GUI
             palThongTin.Controls.Add(thoiKhoaBieu);
             thoiKhoaBieu.Show();
         }
+        private void btnGiaoVien_Click(object sender, EventArgs e)
+        {
+            palThongTin.Controls.Clear();
+            frmGiaoVien giaoVien = new frmGiaoVien();
+            giaoVien.Dock = DockStyle.Fill;
+            palThongTin.Controls.Add(giaoVien);
+            giaoVien.Show();
+        }
     }
 }
