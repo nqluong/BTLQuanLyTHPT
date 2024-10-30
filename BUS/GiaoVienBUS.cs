e23ab40a13ec4f6ec2b46b7188d5103bf044dc0f
trantoan11 <148558306+trantoan11@users.noreply.github.com>
2024-10-30 21:24:22 +0700

    Update GiaoVienBUS.cs

--- BUS/GiaoVienBUS.cs
+++ BUS/GiaoVienBUS.cs
@@ -15,5 +15,25 @@ namespace BUS
         {
             return giaoVienDAL.GetGiaoVienByMaTK(maTK);
         }
-    }
+        public DataTable GetGiaoVien()
+        {
+            return giaoVienDAL.GetGiaoVien();
+        }
+        public bool DeleteGiaoVien(string maGV)
+        {
+            return giaoVienDAL.DeleteGiaoVien(maGV);
+        }
+        public bool AddGiaoVien(GiaoVien giaoVien)
+        {
+            return giaoVienDAL.AddGiaoVien(giaoVien);
+        }
+        public bool UpdateGiaoVien(GiaoVien giaoVien)
+        {
+            if (giaoVienDAL.CheckDuplicateGiaoVien(giaoVien))
+            {
+            throw new Exception("Dữ liệu giáo viên bị trùng lặp với giáo viên khác.");
+        }
+            return giaoVienDAL.UpdateGiaoVien(giaoVien);
+        }
+    }    
 }
